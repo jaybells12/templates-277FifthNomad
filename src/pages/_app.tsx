3588f879b722228f901baab16292f765967de7b8
James Bellomy <jay.bells12@gmail.com>
2023-08-09 23:36:25 -0700

    Add meta data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,32 @@
-import theme from "../theme";
-import { ChakraProvider } from "@chakra-ui/react";
-import type { AppProps } from "next/app";
-import Head from "next/head";
+import theme from '../theme'
+import { ChakraProvider } from '@chakra-ui/react'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1'
+          key='viewport'
+        />
+        <meta
+          name='title'
+          content='277 Fifth Nomad Template'
+          key='title'
+        />
+        <meta
+          name='description'
+          content='A templatized version of a pre-existing site'
+          key='description'
+        />
       </Head>
       <ChakraProvider theme={theme}>
         <Component {...pageProps} />
       </ChakraProvider>
     </>
-  );
+  )
 }
+
