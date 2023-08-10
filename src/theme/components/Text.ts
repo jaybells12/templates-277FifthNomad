572eb3a10d106c8546bbe0878738bc3ff5872f6d
James Bellomy <jay.bells12@gmail.com>
2023-08-10 09:56:52 -0700

    Modify banner variant text element width

--- src/theme/components/Text.ts
+++ src/theme/components/Text.ts
@@ -1,46 +1,46 @@
 import { defineStyle, defineStyleConfig } from '@chakra-ui/react'
 
 const contact = defineStyle({
-  fontFamily: "PortraitText",
-  fontWeight: "light",
-  fontSize: "clamp(0.875rem, 3vw, 1rem)",
-  lineHeight: "1.7",
-  whiteSpace: "pre",
-  letterSpacing: "0.2px",
-  textAlign: ["center", null, "left"]
+  fontFamily: 'PortraitText',
+  fontWeight: 'light',
+  fontSize: 'clamp(0.875rem, 3vw, 1rem)',
+  lineHeight: '1.7',
+  whiteSpace: 'pre',
+  letterSpacing: '0.2px',
+  textAlign: ['center', null, 'left'],
 })
 
 const footer = defineStyle({
-  fontFamily: "PortraitText",
-  fontSize: "0.625rem",
-  fontWeight: "light",
-  lineHeight: "1.5",
-  textAlign: "left",
+  fontFamily: 'PortraitText',
+  fontSize: '0.625rem',
+  fontWeight: 'light',
+  lineHeight: '1.5',
+  textAlign: 'left',
 })
 
 const features = defineStyle({
-  fontFamily: "PortraitText",
-  fontSize: "clamp(0.75rem, 2.5vw, 0.875rem)",
-  letterSpacing: "2px",
+  fontFamily: 'PortraitText',
+  fontSize: 'clamp(0.75rem, 2.5vw, 0.875rem)',
+  letterSpacing: '2px',
 })
 
 const card = defineStyle({
-  fontSize: "clamp(0.875rem, 3vw, 1.0625rem)",
-  lineHeight: "1.5",
-  fontWeight: "normal",
+  fontSize: 'clamp(0.875rem, 3vw, 1.0625rem)',
+  lineHeight: '1.5',
+  fontWeight: 'normal',
 })
 
 const banner = defineStyle({
-  color: "white",
-  fontSize: "1.125rem",
-  paddingBlock: ["2rem", null, "3rem"],
-  width: "60%",
-  minWidth: "314px",
-  margin: "0 auto",
-  lineHeight: "1.4",
-  letterSpacing: "2.7px",
-  opacity: "0.6",
-  textAlign: ["center", null, "left"]
+  color: 'white',
+  fontSize: '1.125rem',
+  paddingBlock: ['2rem', null, '3rem'],
+  width: 'fit-content',
+  minWidth: '314px',
+  margin: '0 auto',
+  lineHeight: '1.4',
+  letterSpacing: '2.7px',
+  opacity: '0.6',
+  textAlign: ['center', null, 'left'],
 })
 
 export default defineStyleConfig({
